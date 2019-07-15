1916d384f9db86b88cde2ec3c642434e60ec9fbe
zhaoyongfang <yongfang@qq.com>
2019-07-15 14:03:58 +0800

    admin

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import {Button, message} from 'antd'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {BrowserRouter,HashRouter,Switch,Route } from "react-router-dom"
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
+/*
+应用根组件
+ */
+class App extends Component {
+
+  handleClick = () => {
+    message.success('成功啦...');
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" component={Login}/>
+          <Route path="/" component={Admin}/>
+        </Switch>
+      </BrowserRouter> 
+    )
+  }
 }
 
-export default App;
+export default App
